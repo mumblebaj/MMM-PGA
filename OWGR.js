e9f4d067fe5defc6f458de1b1ae91dca0db288f8
dathbe <github@beffa.us>
2024-05-16 12:56:49 -0700

    Update OWGR.js
    add catch for too many requests

--- OWGR.js
+++ OWGR.js
@@ -25,20 +25,24 @@ module.exports = {
             rankings: []
         };
         var payload = data;
-        if (payload.results.rankings.length > 1) {
-            for (var i = 0; i < payload.results.rankings.length; i++) {
-		flagName = payload.results.rankings[i].player_name.replace(/\s/g, '');
-                owgrRanking.rankings.push({
-                    "name": payload.results.rankings[i].player_name,
-                    "curPosition": payload.results.rankings[i].position,
-                    "lwPosition": "1",
-                    "points": payload.results.rankings[i].total_points,
-                    "flagUrl": flags.getFlagURL(flagName)
-                });
-                if (i == maxPlayers)
-                    break;
-            }
-        } 
+	if (payload.message == "Too many requests") {
+		console.log(payload);
+	} else {
+	        if (payload.results.rankings.length > 1) {
+	            for (var i = 0; i < payload.results.rankings.length; i++) {
+			flagName = payload.results.rankings[i].player_name.replace(/\s/g, '');
+	                owgrRanking.rankings.push({
+	                    "name": payload.results.rankings[i].player_name,
+	                    "curPosition": payload.results.rankings[i].position,
+	                    "lwPosition": "1",
+	                    "points": payload.results.rankings[i].total_points,
+	                    "flagUrl": flags.getFlagURL(flagName)
+	                });
+	                if (i == maxPlayers)
+	                    break;
+	            }
+	        } 
+	}
 		callback(owgrRanking);
     }
 
